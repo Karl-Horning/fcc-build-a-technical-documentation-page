58df4a93eafeebff7e7d5dab4024ea5f8236a0df
Karl Horning <k.horning@me.com>
2018-10-01 11:38:29 +0100

    Ticked off completed user story in README; updated the code HTML and styles; added SASS to manage the styles more swiftly; created a JS file whose styling is being used as the basis of the code CSS styles

---
+++ resources/js/script.js
@@ -0,0 +1,38 @@
+function greetMe(yourName) {
+    alert("Hello " + yourName);
+}
+greetMe("World");
+
+var x = 42
+let y = 13
+
+if (true) {
+    var x = 5;
+  }
+  console.log(x);  // 
+  
+  const PI = 3.14;
+
+const MY_OBJECT = {"key": "value"};
+MY_OBJECT.key = "otherValue";
+
+if (condition_1) {
+    statement_1;
+} else if (condition_2) {
+    statement_2;
+} else if (condition_n) {
+    statement_n;
+} else {
+    statement_last;
+}
+
+if ((x = y)) {
+    /* statements here */
+}
+
+var n = 0;
+var x = 0;
+while (n < 3) {
+    n++;
+    x += n;
+}
